5bd9b26ac1c5314c906a0e125ef4c3ec1b8418eb
Kevin-Chan279 <Kevin-Chan279@users.noreply.github.com>
2025-05-23 09:21:05 +0000

    deploy: 571fae156fc1cdd3265420376a802e2225cc2e5e

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747991990',resources: [
+    cacheName: 'chirpy-1747992065',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
