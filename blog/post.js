2cd765b187f6ae45257b9cc39f24607c8f3ad557
Kevin-Chan279 <hover_feel@hotmail.com>
2025-05-28 16:30:23 +0800

    Create post.js
    Signed-off-by: Kevin-Chan279 <hover_feel@hotmail.com>

---
+++ blog/post.js
@@ -0,0 +1,12 @@
+const posts = [
+  {
+    title: "我的第一篇文章",
+    filename: "2025-05-23-first-post.html",
+    date: "2025-05-23"
+  },
+  {
+    title: "第二篇文章",
+    filename: "2025-05-28-second-post.html",
+    date: "2025-05-28"
+  }
+];
